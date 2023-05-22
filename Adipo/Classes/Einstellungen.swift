a8b0580d9c9277b32b6aaf9bfe1f4a13b44f054f
Volker Schering <mail@volker-schering.de>
2023-05-22 19:09:03 +0200

    Deactivate UserDefaults

--- Adipo/Classes/Einstellungen.swift
+++ Adipo/Classes/Einstellungen.swift
@@ -26,31 +26,31 @@ class Einstellungen {
    var geschlecht: String {
       didSet {
          print(String(format: "Geschlecht gesetzt: %@", geschlecht))
-         UserDefaults.standard.set(self.geschlecht, forKey: "geschlecht")
+//         UserDefaults.standard.set(self.geschlecht, forKey: "geschlecht")
       }
    }
    var gewicht: Int {
       didSet {
          print(String(format: "Gewicht gesetzt: %d", gewicht))
-         UserDefaults.standard.set(self.gewicht, forKey: "gewicht")
+//         UserDefaults.standard.set(self.gewicht, forKey: "gewicht")
       }
    }
    var größe: Int {
       didSet {
          print(String(format: "Größe gesetzt: %d", größe))
-         UserDefaults.standard.set(self.größe, forKey: "größe")
+//         UserDefaults.standard.set(self.größe, forKey: "größe")
       }
    }
    var taillenumfang: Int {
       didSet {
          print(String(format: "Taillenumfang gesetzt: %d", taillenumfang))
-         UserDefaults.standard.set(self.taillenumfang, forKey: "taillenumfang")
+//         UserDefaults.standard.set(self.taillenumfang, forKey: "taillenumfang")
       }
    }
    var hüftumfang: Int {
       didSet {
          print(String(format: "Hüftumfang gesetzt: %d", hüftumfang))
-         UserDefaults.standard.set(self.hüftumfang, forKey: "hüftumfang")
+//         UserDefaults.standard.set(self.hüftumfang, forKey: "hüftumfang")
       }
    }
 
@@ -61,34 +61,34 @@ class Einstellungen {
       self.taillenumfang = md.taillenumfang
       self.hüftumfang = md.hüftumfang
       
-      self.loadUserDefaults()
+//      self.loadUserDefaults()
    }
    
-   internal func loadUserDefaults() {
-      print(String("Load User Defaults"))
-      if isKeyPresentInUserDefaults(key: "geschlecht") {
-         self.geschlecht = UserDefaults.standard.string(forKey: "geschlecht")!
-      } else { self.geschlecht = md.geschlecht }
-      
-      if isKeyPresentInUserDefaults(key: "gewicht") {
-         self.gewicht = UserDefaults.standard.integer(forKey: "gewicht")
-      } else { self.gewicht = md.gewicht }
-      
-      if isKeyPresentInUserDefaults(key: "größe") {
-         self.größe = UserDefaults.standard.integer(forKey: "größe")
-      } else { self.größe = md.größe }
-      
-      if isKeyPresentInUserDefaults(key: "taillenumfang") {
-         self.taillenumfang = UserDefaults.standard.integer(forKey: "taillenumfang")
-      } else { self.taillenumfang = md.taillenumfang }
-      
-      if isKeyPresentInUserDefaults(key: "hüftumfang") {
-         self.hüftumfang = UserDefaults.standard.integer(forKey: "hüftumfang")
-      } else { self.hüftumfang = md.hüftumfang }
-      print("UserDefaults loaded")
-   }
-   
-   internal func isKeyPresentInUserDefaults(key: String) -> Bool {
-       return UserDefaults.standard.object(forKey: key) != nil
-   }
+//   internal func loadUserDefaults() {
+//      print(String("Load User Defaults"))
+//      if isKeyPresentInUserDefaults(key: "geschlecht") {
+//         self.geschlecht = UserDefaults.standard.string(forKey: "geschlecht")!
+//      } else { self.geschlecht = md.geschlecht }
+//
+//      if isKeyPresentInUserDefaults(key: "gewicht") {
+//         self.gewicht = UserDefaults.standard.integer(forKey: "gewicht")
+//      } else { self.gewicht = md.gewicht }
+//
+//      if isKeyPresentInUserDefaults(key: "größe") {
+//         self.größe = UserDefaults.standard.integer(forKey: "größe")
+//      } else { self.größe = md.größe }
+//
+//      if isKeyPresentInUserDefaults(key: "taillenumfang") {
+//         self.taillenumfang = UserDefaults.standard.integer(forKey: "taillenumfang")
+//      } else { self.taillenumfang = md.taillenumfang }
+//
+//      if isKeyPresentInUserDefaults(key: "hüftumfang") {
+//         self.hüftumfang = UserDefaults.standard.integer(forKey: "hüftumfang")
+//      } else { self.hüftumfang = md.hüftumfang }
+//      print("UserDefaults loaded")
+//   }
+//
+//   internal func isKeyPresentInUserDefaults(key: String) -> Bool {
+//       return UserDefaults.standard.object(forKey: key) != nil
+//   }
 }
